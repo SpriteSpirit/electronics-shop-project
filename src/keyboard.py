1e16f768506bcc362c6159377b78aca541b261cc
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-01-25 13:55:27 +0300

    HW5: keyboard.py: - реализован класс Keyboard + наследование - создан геттер для поля self._language

---
+++ src/keyboard.py
@@ -0,0 +1,13 @@
+from src.item import Item
+
+
+class Keyboard(Item):
+
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self._language = 'EN'
+
+    @property
+    def language(self):
+        return self._language
+
